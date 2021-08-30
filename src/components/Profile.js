9a501f2578dc44fe9a318cb8011f6e7367410902
Sander Iwase <sanderiw@gmail.com>
2021-08-30 17:37:55 -0300

    Done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,8 @@ function Profile () {
         <div className="card mb-3" style={{width: 18 + 'rem'}}>
             <img src={profile} className="card-img-top" alt="do"/>
             <div className="card-body">
-                <p className="card-text">Lorem ipsum</p>
+                <h5 className="card-title">Cute Puppy</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae aliquam nunc. Praesent arcu tellus, tempor at gravida id, venenatis porttitor dolor. Cras vitae ipsum id sem ullamcorper congue a id quam.</p>
             </div>
         </div>
     );
